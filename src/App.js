97f79ed4668e401985f9f1d919cb7bb41bf655ad
DylandeBruijn <dylandebruyn1993@gmail.com>
2021-06-14 22:19:21 +0200

    Update state trigger rerender

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
-import React, { useEffect } from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import PokemonCard from "./components/PokemonCard";
 
 function App() {
+	const [pokemonNames, setPokemonNames] = useState(null);
+
+	useEffect(() => {
+		async function fetchPokemonNames() {
+			const result = await axios.get(
+				"https://pokeapi.co/api/v2/pokemon?limit=20"
+			);
+			setPokemonNames(result.data.results);
+		}
+		fetchPokemonNames();
+	}, []);
 	return (
 		<div>
-			<PokemonCard nameOfPokemon="jigglypuff" />
-			<PokemonCard nameOfPokemon="diglett" />
+			{pokemonNames ? (
+				<div>
+					<PokemonCard nameOfPokemon="jigglypuff" />
+					<PokemonCard nameOfPokemon="diglett" />
+				</div>
+			) : (
+				<h3>Loading...</h3>
+			)}
 		</div>
 	);
 }
