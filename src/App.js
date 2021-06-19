08472c040b7ebfeec6f9efbfd2f66275ce26255e
DylandeBruijn <dylandebruyn1993@gmail.com>
2021-06-19 13:35:42 +0200

    New correct functionality for app

--- src/App.js
+++ src/App.js
@@ -1,37 +1,66 @@
+import React, { useState, useEffect } from "react";
 import axios from "axios";
-import React, { useEffect, useState } from "react";
+import Pokemon from "./components/pokemon/Pokemon";
+import Button from "./components/button/Button";
+import logo from "./assets/logo.png";
 import "./App.css";
-import PokemonCard from "./components/PokemonCard";
-import PaginationButton from "./components/PaginationButton";
 
 function App() {
-	const [pokemonNames, setPokemonNames] = useState(null);
+	const [pokemons, setPokemons] = useState([]);
+	const [endpoint, setEndpoint] = useState(
+		"https://pokeapi.co/api/v2/pokemon/"
+	);
+	const [loading, toggleLoading] = useState(false);
+	const [error, setError] = useState(false);
 
 	useEffect(() => {
-		async function fetchPokemonNames() {
-			const result = await axios.get(
-				"https://pokeapi.co/api/v2/pokemon?limit=20"
-			);
-			setPokemonNames(result.data.results);
+		async function fetchData() {
+			toggleLoading(true);
+			setError(false);
+
+			try {
+				const { data } = await axios.get(endpoint);
+				setPokemons(data);
+			} catch (e) {
+				console.error(e);
+				setError(true);
+			}
+
+			toggleLoading(false);
 		}
-		fetchPokemonNames();
-	}, []);
+
+		fetchData();
+	}, [endpoint]);
+
 	return (
-		<main className="pokedex-wrapper">
-			<section className="next-previous-buttons">
-				<PaginationButton buttonText="Previous" />
-				<PaginationButton buttonText="Next" />
-			</section>
-			{pokemonNames ? (
-				<div className="pokedex-container">
-					{pokemonNames.map((pokemonName) => {
-						return <PokemonCard nameOfPokemon={pokemonName.name} />;
-					})}
-				</div>
-			) : (
-				<h3>Loading...</h3>
+		<div className="poke-deck">
+			{pokemons && (
+				<>
+					<img alt="logo" width="400px" src={logo} />
+					<section className="button-bar">
+						<Button
+							disabled={!pokemons.previous}
+							clickHandler={() => setEndpoint(pokemons.previous)}
+						>
+							Vorige
+						</Button>
+						<Button
+							disabled={!pokemons.next}
+							clickHandler={() => setEndpoint(pokemons.next)}
+						>
+							Volgende
+						</Button>
+					</section>
+
+					{pokemons.results &&
+						pokemons.results.map((pokemon) => {
+							return <Pokemon key={pokemon.name} endpoint={pokemon.url} />;
+						})}
+				</>
 			)}
-		</main>
+			{loading && <p>Loading...</p>}
+			{error && <p>Er ging iets mis bij het ophalen van de data...</p>}
+		</div>
 	);
 }
 
